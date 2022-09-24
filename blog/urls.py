21641b9d7e4b1b7bb3bf6b3b2171fdf804c84b51
sameer29 <sameer12982@gmail.com>
2022-09-24 16:02:30 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
-    path('createblog/',views.createblog),
-    path('myblog/',views.myblog)
+    path('http://samsblogapp.herokuapp.com/createblog/',views.createblog),
+    path('http://samsblogapp.herokuapp.com/myblog/',views.myblog)
 ]
